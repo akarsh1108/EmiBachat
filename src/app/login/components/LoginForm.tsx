4b4d4a6587896e7ff754c298a2dd357d15893b62
akarsh1108 <akarshpriyadarshancp@gmail.com>
2024-11-24 22:31:32 +0530

    login

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -89,7 +89,7 @@ export default function LoginForm() {
         </Button>
 
         <p className="text-sm text-center text-gray-500 mt-4">
-          Don't have an account?{" "}
+          Don`&apos;`t have an account?{" "}
           <a href="/register" className="text-blue-500 hover:underline">
             Register
           </a>
